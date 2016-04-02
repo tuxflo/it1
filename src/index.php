f629a8cc5ff2b9d5b10dcb67814bb41549d85ac6
tuxflo <flo.hennig@gmail.com>
2016-04-02 13:47:47 +0200

    change default language and add authors

--- src/index.php
+++ src/index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 
 <head>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Beleg für das Modul Internettechnologien 1">
+    <meta name="author" content="Florian Hennig und Jana Will">
 
     <title>The next generation Blog engine</title>
 
