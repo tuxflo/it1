0dce6c81658d9e97d8ff8b329f28204da85df961
tuxflo <flo.hennig@gmail.com>
2016-04-05 20:27:47 +0200

    add index.php layout example

--- src/home.php
+++ src/home.php
@@ -16,45 +16,17 @@
 
     <!-- Custom CSS -->
     <link href="css/logo-nav.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
 
 <body>
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">
-                    <img src="http://placehold.it/150x50&text=Logo" alt="">
-                </a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin<span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="newpost.php">New Post</a></li>
-                      <li><a href="#">Edit Post</a></li>
-                      <li role="separator" class="divider"></li>
-                      <li><a href="#">Upload Images</a></li>
-                    </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
-
-    <!-- Page Content -->
 <div class="row-fluid top30 pagetitle">
   
   <div class="container">
@@ -168,32 +140,4 @@
     </div>
   </div>
 </div>
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-<script type="text/javascript">
-$(document).ready(function() {
-    var x_timer;    
-    $("#url").keyup(function (e){
-        clearTimeout(x_timer);
-        var user_name = $(this).val();
-        x_timer = setTimeout(function(){
-            check_username_ajax(user_name);
-        }, 1000);
-    });
-
-function check_username_ajax(username){
-    $("#user-result").html('<img src="ajax-loader.gif" />');
-    $.post('username-checker.php', {'username':username}, function(data) {
-      $("#user-result").html(data);
-    });
-}
-});
-</script>
 </body>
-
-</html>
