3bb8913a9fc8881819029e1f445a96d6e8cd4a96
chibi4 <chibi@fn.de>
2016-04-11 21:29:52 +0200

    adds simple image upload script (verified, that only pictures are  allowed, stores them in /img) and displays all images in /img delete button doesn't do anything yet

--- src/upload.php
+++ src/upload.php
@@ -22,41 +22,47 @@ werden.
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-    <!-- action refers to the script that will be executed, the entry
-    specifies the current file(name) -->
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" enctype="multipart/form-data">
-      <div class="input-group">
-        <input type="hidden" name="MAX_FILE_SIZE" value="2000000" />
-        <input type="file" name="file" />
-        <input class="btn btn-primary" type="submit" value="Upload" />
-      </div>
-    </form>
-    <?php
-       if (isset($_POST["file"]) && ! $_FILES["file"]["error"]){
-         print "file uploading";
-         $imginfo = getimagesize($_FILES["file"]["tmp_name"]);
-         if ($imginfo){ // file is image
-           print "file is image";
-           $allowed_mime = array( "image/jpeg" => "jpg", "image/png" => "png", "image/gif" => "gif",);
-           if (isset($allowed_mime[$imginfo["mime"]])){
-             // delete unallowed characters from filename, add correct mimetype 
-             print "file is allowed mime"; $fname = basename($_FILES["file"]["name"]);
-             $fname = preg_replace("/\.(jpe?g|gif|png$/i)", "", $fname);
-             $fname = preg_replace("/[^a-zA-Z0-9-_]/", "", $fname);
-             $path = "img/" . $fname . $imginfo["mime"];
-             while (file_exists($path)){
-               $fname = $fname . "_1";
-               $path = "img/" . $fname . $imginfo["mime"];
-             }
-             // copy file to img/
-             if (move_uploaded_file($_FILES["file"]["tmp_name"], $path)){
-               print "fileuploaded successfully";
-             }
+        <!-- action refers to the script that will be executed, the entry specifies the current file(name) -->
+
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post" enctype="multipart/form-data">
+          <div class="input-group">
+            <!--<input type="hidden" name="MAX_FILE_SIZE" value="2000000" /> -->
+            <input type="file" name="f" />
+            <input class="btn btn-primary" type="submit" value="Upload" />
+          </div>
+        </form>
+
+        <!-- Image Upload Script -->
+        <?php
+           if (isset($_FILES["f"]) && ($_FILES["f"]["error"] == 0)){
+               
+               $imginfo = getimagesize($_FILES["f"]["tmp_name"]);
+
+               if ($imginfo){ // file is image
+                   $allowed_mime = array( "image/jpeg" => "jpg", "image/png" => "png", "image/gif" => "gif",);
+
+                   if (isset($allowed_mime[$imginfo["mime"]])){
+                       // delete unallowed characters from filename, add correct mimetype 
+                       $fname = basename($_FILES["f"]["name"]); 
+                       $type = "." . $allowed_mime[$imginfo["mime"]];
+
+                       $fname = preg_replace("/\.(jpe?g|gif|png)$/i", "", $fname); 
+                       $fname = preg_replace("/[^a-zA-Z0-9_-]/", "", $fname); 
+                       $path = "img/" . $fname . $type;
+                       while (file_exists($path)){
+                           $fname = $fname . "_1";
+                           $path = "img/" . $fname . $type;
+                       }
+                       
+                       // copy file to img/
+                       if (@move_uploaded_file($_FILES["f"]["tmp_name"], $path)){
+                           print "fileuploaded successfully\n";
+                       }
+                   }
+               }
            }
-         }
-       } print "error uploading / no file selected";
-    ?>
-    </div>
+?>
+      </div>
     </div>
   </div>
   
